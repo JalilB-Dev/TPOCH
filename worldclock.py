d99a60aee01c7f231b3d8b9706842e199b6c1e6c
JalilB-Dev <jalilb.dev@gmail.com>
2026-05-01 20:33:57 -0400

    Initial commit: World Clock with 12 major cities and auto-refresh

---
+++ worldclock.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+from zoneinfo import ZoneInfo
+import time
+import os
+
+def show_time(city, timezone):
+	currenttime = datetime.now(ZoneInfo(timezone))
+	print(city + ": " + time.strftime("%A, %B %d %I:%M %p %Z"))
+
+
+
+cities = [
+	{"name": "New York", "timezone": "America/New_York"},
+	{"name": "London", "timezone": "Europe/London"},
+	{"name": "Tokyo", "timezone": "Asia/Tokyo"},
+	{"name": "Los Angeles", "timezone": "America/Los_Angeles"},
+	{"name": "Paris", "timezone": "Europe/Paris"},
+	{"name": "Sydney", "timezone": "Australia/Sydney"},
+	{"name": "Dubai", "timezone": "Asia/Dubai"},
+	{"name": "Singapore", "timezone": "Asia/Singapore"},
+	{"name": "Hong Kong", "timezone": "Asia/Hong_Kong"},
+	{"name": "Kolkata", "timezone": "Asia/Kolkata"},
+	{"name": "Sāo Paulo", "timezone": "America/Sao_Paulo"},
+	{"name": "Moscow", "timezone": "Europe/Moscow"},
+]
+
+while True:
+	for city in cities:
+		show_time(city["name"], city["timezone"])
+	time.sleep(60)
+	os.system("clear")
+	
